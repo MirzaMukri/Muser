183f6caeb1468f58e718e4252ab3b90c68f8e1ea
Mirza Mukri <mukri@planetbn.com>
2016-12-17 17:22:59 +0800

    Muser Project Started

---
+++ src/com/mukri/muser/Muser.java
@@ -0,0 +1,37 @@
+package com.mukri.muser;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+/**
+ * CopyRighted by DoomGary / Mukri
+ * Please do not edit or copy without permissions.
+ * Made on: 5:15:53 PM 
+ */
+
+public class Muser extends JavaPlugin {
+	
+	public Muser instance;
+	
+	public void onEnable() {
+		instance = this;
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public Muser getIns() {
+		return instance;
+	}
+	
+	public void listen() {
+		
+	}
+	
+	public void commands() {
+		
+	}
+
+}
---
+++ src/com/mukri/muser/mysql/MySql.java
@@ -0,0 +1,14 @@
+package com.mukri.muser.mysql;
+
+
+/**
+ * CopyRighted by DoomGary / Mukri
+ * Please do not edit or copy without permissions.
+ * Made on: 5:16:13 PM 
+ */
+
+public class MySql {
+	
+	
+
+}
